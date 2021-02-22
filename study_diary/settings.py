6fb9c286f6933c499d1eeb0192cf78f9cc6bcdac
HenryR88 <henryromanenko@gmail.com>
2021-02-22 01:33:28 +0000

    ..

--- study_diary/settings.py
+++ study_diary/settings.py
@@ -132,7 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR)
 
 # Heroku settings.
 import django_heroku
-
 django_heroku.settings(locals())
 
-DEBUG = os.environ.get('DEBUG', False)
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
