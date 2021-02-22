d352f76e81b7e83783e4edf129285b1039389101
HenryR88 <henryromanenko@gmail.com>
2021-02-22 01:10:23 +0000

    Set DEBUG based on environment variables.

--- study_diary/settings.py
+++ study_diary/settings.py
@@ -133,3 +133,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR)
 # Heroku settings.
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
